349b344b6c0611c0d643d273dbe8432441d07fec
Rogério Piveto <127915397+pivetoo@users.noreply.github.com>
2025-11-02 23:57:05 -0300

    melhorias switch

--- d-rts/dist/index.js
+++ d-rts/dist/index.js
@@ -8862,7 +8862,7 @@ const cB = x.forwardRef(({ className: e, ...t }, r) => /* @__PURE__ */ A(
   uT,
   {
     className: te(
-      "peer inline-flex h-5 w-9 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent shadow-sm transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input",
+      "peer inline-flex h-5 w-9 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent shadow-sm transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-green-500 data-[state=unchecked]:bg-red-500",
       e
     ),
     ...t,
